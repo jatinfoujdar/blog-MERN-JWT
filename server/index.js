ff323218216790fe767f6b00b5e21b53595a6eef
Jatin Foujdar <jatin321001@gmail.com>
2023-05-06 22:56:08 +0530

    dotenv commit

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 import express from "express";
 import Connection from "./Database/db.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express()
 const port = 4000
 
-
-
-
 app.listen(port, () => {
-  console.log(` app listening on port ${port}`)
+  console.log(` App listening on port ${port}`)
 })
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
 
-Connection();
+Connection(USERNAME,PASSWORD);
