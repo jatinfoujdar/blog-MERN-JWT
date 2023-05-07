75cc61ea8e7cd61297b735a7eb03847b7088df30
Jatin Foujdar <jatin321001@gmail.com>
2023-05-07 15:41:40 +0530

    api  message commit

--- client/src/Config/constant.js
+++ client/src/Config/constant.js
@@ -1,1 +1,25 @@
-export const LogoIMG = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEVDOKbmDKHqL-bLn1rdci2pQW9rlCt3FvWQ&usqp=CAU"
+export const LogoIMG = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEVDOKbmDKHqL-bLn1rdci2pQW9rlCt3FvWQ&usqp=CAU";
+//api notification message
+
+export const API_NOTIFICATION_MESSAGES = {
+    loading:{
+        title: "loading...",
+        message: "data is loading"
+    },
+    success:{
+       title: "success ",
+       message: " data successfully"
+    },
+    resFailure:{
+        title: "Error",
+        message: "Error occured while fetching while parsing res data"
+    },
+    reqFailure:{
+        title: "Error",
+        message: "an error occured while parsing req data"
+    },
+    networkError:{
+        title: "Error",
+        message: "unable to connect to server "
+    }
+}
--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { API_NOTIFICATION_MESSAGES } from "../Config/constant";
 
 const API_URL = "http://localhost:4000";
 
@@ -23,10 +24,54 @@ axiosInstance.interceptors.req.use(
 axiosInstance.interceptors.res.use(
     function(res){
        //stop global loader here
+       return processResponce(res)
     },
     function (error){
-        return Promise.reject(error)
+        return Promise.reject(processError(error))
     }
 )
 
+const processResponce = (res) => {
+    if(res?.status === 200){
+        return{isSucess: true, data: Response.data}
+    }else{
+        return{
+        isFailure: true,
+        status: res?.status,
+        msg: response?.msg,
+        code: response?.code,    
+    }
+    }
+}
+
+const processError = (error) =>{
+      if (error.res) {
+        //req made and server responded with a status
+        console.log("error in res",error.toJSON());
+        return{
+            isError :true,
+            msg : API_NOTIFICATION_MESSAGES.resFailure,
+            code: error.res.status
+
+        }
+      } else if(error.req){
+        //req made but no response was received
+        console.log("error in req",error.toJSON());
+        return{
+            isError :true,
+            msg : API_NOTIFICATION_MESSAGES.networkError,
+            code: ""
+
+        }
+      }else{
+        //something happned in setting up
+        console.log("error in res",error.toJSON());
+        return{
+            isError :true,
+            msg : API_NOTIFICATION_MESSAGES.resFailure,
+            code: ""
+        }
+        
+      }
+}
 
