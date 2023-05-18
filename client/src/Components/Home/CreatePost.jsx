a4ba2906fd3805053297a10bfa1ee581b8b5417b
Jatin Foujdar <jatin321001@gmail.com>
2023-05-18 14:54:03 +0530

    create post commit

--- client/src/Components/Home/CreatePost.jsx
+++ client/src/Components/Home/CreatePost.jsx
@@ -1,7 +1,8 @@
-import { Box, FormControl } from '@mui/material'
+import { Box, Button, FormControl,  InputBase, TextareaAutosize } from '@mui/material'
 import React from 'react'
 import { createpostimage } from '../../Config/constant'
 import styled from '@emotion/styled'
+import {AddCircle as Add} from '@mui/icons-material';
 
 const Container = styled(Box)`
 margin: 50px 100px
@@ -13,17 +14,37 @@ const Image = styled("img")({
     objectFit: "cover"
 })
 
+const StyledFormControl = styled(FormControl)`
+
+margin-top: 10px;
+display: flex;
+flex-direction: row;
+`
+const InputTextField = styled(InputBase)`
+flex: 1;
+margin: 0 30px;
+font-size: 25px;
+`
+
 const CreatePost = () => {
   return (
    
     <Container>
         <Image src={createpostimage} alt='creatpost'/>
-        <FormControl>
+        <StyledFormControl>
             <label htmlFor='fileInput'>
-                Input
+                <Add fontSize='large' color='action'/>
             </label>
-            <input type='file'/>
-        </FormControl>
+            <input type='file'
+            id='fileInput'
+            style={{display: 'none'}}/>
+            <InputTextField placeholder='title'/>
+            <Button variant='contained'>Publish</Button>
+        </StyledFormControl>
+        <TextareaAutosize 
+        minRows={5}
+        placeholder='Tell your Story....'
+        />
     </Container>
 
   )
