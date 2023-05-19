dd317d7e8dd216e5494dd2076e20fc7b93fd56f8
Jatin Foujdar <jatin321001@gmail.com>
2023-05-19 17:14:24 +0530

    working upload commit

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,28 +1,20 @@
-import multer from "multer";
-
-import {GridFsStorage} from "multer-gridfs-storage"
-import dotenv from "dotenv";
-
-
-dotenv.config();
-
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
+import multer from 'multer';
+import { GridFsStorage } from 'multer-gridfs-storage';
 
 const storage = new GridFsStorage({
-    URL : `mongodb://${username}:${password}@ac-kbusnl5-shard-00-00.g1o73sw.mongodb.net:27017,ac-kbusnl5-shard-00-01.g1o73sw.mongodb.net:27017,ac-kbusnl5-shard-00-02.g1o73sw.mongodb.net:27017/?ssl=true&replicaSet=atlas-10yuwk-shard-0&authSource=admin&retryWrites=true&w=majority`,
-    option : {useNewUrlParser: true},
-    file:(req,file) => {
-        const match = ["image/png","image/jpg"];
-        if(match.indexOf(file.memeType)=== -1){
-            return `${Date.now()}-blog-${file.originalname}`;
-        }
-        return{
+    url: URL = `mongodb://jatinfoujdar:jatinfoujdar1234@ac-kbusnl5-shard-00-00.g1o73sw.mongodb.net:27017,ac-kbusnl5-shard-00-01.g1o73sw.mongodb.net:27017,ac-kbusnl5-shard-00-02.g1o73sw.mongodb.net:27017/?ssl=true&replicaSet=atlas-10yuwk-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    options: { useNewUrlParser: true },
+    file: (request, file) => {
+        const match = ["image/png", "image/jpg"];
+
+        if(match.indexOf(file.memeType) === -1) 
+            return`${Date.now()}-blog-${file.originalname}`;
+
+        return {
             bucketName: "photos",
             filename: `${Date.now()}-blog-${file.originalname}`
         }
     }
-})
+});
 
-
-export default multer({storage});
+export default multer({storage}); 
